fe956a24497c2d6e0920c6aebfe8de216f468255
TayyabShaban <66559710+TayyabShaban@users.noreply.github.com>
2020-06-09 19:36:22 +0500

    created struct for node and initialized

--- Initial.go
+++ Initial.go
@@ -1,7 +1,8 @@
-package Raft
+package main
 
 import (
-	"time"
+	"fmt"
+	"strconv"
 )
 
 //node struct
@@ -12,10 +13,17 @@ type node struct {
 	totalNode int
 	dataValue string
 	didVote   bool
-	timer     time.Time
+	//timer     time.Time
 }
 
 func main() {
 	//main
+	fmt.Println("main started")
+	var nod [3]node
+	//var i int =0
+	for i := 0; i < 3; i++ {
+		nod[i] = node{"node" + strconv.Itoa(i), "follower", 0, 3, "", false}
+		fmt.Println(nod[i])
+	}
 
 }
